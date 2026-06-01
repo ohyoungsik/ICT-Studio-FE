9b140231a2b7b88dbf01230f57de25f3072c2f7d
youngsik <dudtlr9308@gmail.com>
2026-06-01 18:03:36 +0900

    first commit & react-typescript init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Welcome to React + TypeScript</h1>
+        <p>This workspace is set up for a TypeScript-based React app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
